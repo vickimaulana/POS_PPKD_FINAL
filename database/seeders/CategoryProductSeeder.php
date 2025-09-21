f0b95b3e8708893ebfbc9e410f915c7424b3b0fe
vickimaulana <vmaulana01@gmail.com>
2025-09-22 06:47:12 +0700

    error db seed

--- database/seeders/CategoryProductSeeder.php
+++ database/seeders/CategoryProductSeeder.php
@@ -17,64 +17,17 @@ class CategoryProductSeeder extends Seeder
         Category::insert([
             [
                 'id' => 1,
-                'category_name' => 'Makanan Berat',
+                'category_name' => 'Makanan T',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'id' => 2,
-                'category_name' => 'Makanan Ringan',
+                'category_name' => 'Minuman',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            [
-                'id' => 3,
-                'category_name' => 'Minuman Dingin',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 4,
-                'category_name' => 'Minuman Panas',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 5,
-                'category_name' => 'Dessert',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 6,
-                'category_name' => 'Fast Food',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 7,
-                'category_name' => 'Seafood',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 8,
-                'category_name' => 'Asian Food',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 9,
-                'category_name' => 'Western Food',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 10,
-                'category_name' => 'Italian Food',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+             
         ]);
 
         Product::factory()->count(50)->create();
